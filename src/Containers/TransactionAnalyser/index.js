bd21ebbc723d98766bff5b62b1f746a119340610
Mithun Prem <mithunprem19@gmail.com>
2019-09-28 14:45:35 +1000

    Added support to parse the csv file and to set the csv data to component state.

--- src/Containers/TransactionAnalyser/index.js
+++ src/Containers/TransactionAnalyser/index.js
@@ -1,9 +1,31 @@
 import React, { Component  } from "react"
+import Papa from 'papaparse';
+import transactionscsv from '../../Data/transactions.csv';
 
 class TransactionAnalyser extends Component {
 
+  state = {
+    transactions: []
+  }
+
+  componentDidMount() {
+    Papa.parse(transactionscsv, {
+      header: true,
+      download: true,
+      skipEmptyLines: true,
+      dynamicTyping: true,
+      complete: this.setTransactionsToState
+    })
+  }
+
+  setTransactionsToState = ({ data: transactions }) => {
+    if( transactions && transactions.length > 0 ) {
+      this.setState({ transactions });
+    }
+  }
+
   render() {
-    return <h1>Transaction Analyser</h1>;
+    return <h3>Transaction Analyser</h3>;
   }
 }
 
