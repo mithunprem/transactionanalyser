ced69206af07e4ff9c1fe1a4bdaa8bf26792d95a
Mithun Prem <mithunprem19@gmail.com>
2019-09-28 17:17:01 +1000

    Added value and onChange handler to the input field.

--- src/Components/Input/index.js
+++ src/Components/Input/index.js
@@ -1,13 +1,13 @@
 import React, { Fragment } from 'react';
 import './input.scss';
 
-const Input = ({ type = "text", placeholder, name }) => {
+const Input = ({ type = "text", placeholder, name, value, onChange = () => { } }) => {
   return (
     <Fragment>
       <div className="input">
         <label htmlFor={name}> {placeholder}</label>
         <br/>
-        <input type={type} name={name} />
+        <input type={type} name={name} value={value} onChange={onChange} />
       </div>
     </Fragment>
   )
--- src/Containers/TransactionAnalyser/index.js
+++ src/Containers/TransactionAnalyser/index.js
@@ -8,7 +8,13 @@ import './transactionAnalyser.scss'
 class TransactionAnalyser extends Component {
 
   state = {
-    transactions: []
+    transactions: [],
+    form: {
+      accountId: "ACC334455",
+      fromDate: "2018-10-20T00:00",
+      toDate: "2018-10-20T19:00"
+    },
+    balance: 0
   }
 
   componentDidMount() {
@@ -28,15 +34,16 @@ class TransactionAnalyser extends Component {
   }
 
   render() {
+    const { form: { accountId, fromDate, toDate }, balance } = this.state;
     return (
       <Fragment>
         <div className="transaction-analyser">
           <h3>Transaction Analyser</h3>
           <p>Please enter the following details.</p>
           <form onSubmit={this.calculate}>
-            <Input type="text" placeholder="Account ID" name="accountId" />
-            <Input type="datetime-local" placeholder="From" name="fromDate" />
-            <Input type="datetime-local" placeholder="To" name="toDate" />
+            <Input type="text" placeholder="Account ID" name="accountId" value={accountId} onChange={this.handleChange} />
+            <Input type="datetime-local" placeholder="From" name="fromDate" value={fromDate} onChange={this.handleChange} />
+            <Input type="datetime-local" placeholder="To" name="toDate" value={toDate} onChange={this.handleChange} />
             <Button type="submit" label="Calculate" />
           </form>
           <p>Click calculate to show the balance</p>
