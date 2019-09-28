b40f0f128411efe8724cdd5ed652253a51e7cab2
Mithun Prem <mithunprem19@gmail.com>
2019-09-28 13:43:12 +1000

    Modified the App.js to show the Header and the Container view.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import Header from './Components/Header';
+import TransactionAnalyser from './Containers/TransactionAnalyser';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Header />
+      <TransactionAnalyser />
     </div>
   );
 }
