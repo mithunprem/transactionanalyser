7e38ba93afd859e506a955d0c9c93e9410f17ccd
Mithun Prem <mithunprem19@gmail.com>
2019-09-28 13:42:51 +1000

    Designed a container to that will be the landing page of the application.

---
+++ src/Containers/TransactionAnalyser/index.js
@@ -0,0 +1,10 @@
+import React, { Component  } from "react"
+
+class TransactionAnalyser extends Component {
+
+  render() {
+    return <h1>Transaction Analyser</h1>;
+  }
+}
+
+export default TransactionAnalyser;
