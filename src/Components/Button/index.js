394a167bd6019344adfd7a7b5464be42c42e26b5
Mithun Prem <mithunprem19@gmail.com>
2019-09-28 17:17:17 +1000

    Added an onClick handler to the button component.

--- src/Components/Button/index.js
+++ src/Components/Button/index.js
@@ -1,10 +1,10 @@
 import React, { Fragment  } from 'react';
 import './button.scss';
 
-const Button = ({ type = "text", label }) => {
+const Button = ({ type = "text", label, onClick = () => { } }) => {
   return (
     <Fragment>
-      <button className="button" type={type}>{label}</button>
+      <button className="button" type={type} onClick={onClick}>{label}</button>
     </Fragment>
   )
 }
