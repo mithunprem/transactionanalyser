c23d689844775a1fddb2243ce92c55c137cc57a4
Mithun Prem <mithunprem19@gmail.com>
2019-09-29 12:57:00 +1000

    Refactored the balanceCalculator util function to return a success/error status and a message. This will help to show a proper message to user when there are no matching transactions with the given search criteria.

--- src/Utils/balanceCalculator.js
+++ src/Utils/balanceCalculator.js
@@ -1,25 +1,33 @@
 import moment from 'moment';
 
 const calculate = ( transactions, formData ) => {
-  let balance = 0;
+  let balance = 0, status = '', message = '';
   const { accountId, fromDate, toDate } = formData;
 
-  transactions.filter(record => (
-    record.fromAccountId === accountId &&
-    record.createdAt > moment(fromDate) &&
-    record.createdAt < moment(toDate) &&
-    transactions.findIndex(transaction => (
-      transaction.transactionType === "REVERSAL" &&
-      transaction.relatedTransaction === record.transactionId
-    )) === -1
-  )).map(item => {
-    if (item.transactionType === "PAYMENT") balance -= item.amount
-    if (item.transactionType === "REVERSAL") balance += item.amount
-    return true;
-  })
+  transactions =
+    transactions.filter(({ fromAccountId, createdAt, transactionId }) => (
+      fromAccountId === accountId &&
+      createdAt > moment(fromDate) &&
+      createdAt < moment(toDate) &&
+      transactions.findIndex(({ transactionType, relatedTransaction }) => (
+        transactionType === "REVERSAL" &&
+        relatedTransaction === transactionId
+      )) === -1
+  ));
 
-return balance;
+  if (transactions.length > 0) {
+    transactions.forEach(({ transactionType, amount }) => {
+      if (transactionType === "PAYMENT") balance -= amount;
+      if (transactionType === "REVERSAL") balance += amount;
+    });
+    status = 'success';
+    message = 'Balance calculated';
+  } else {
+    status = 'error';
+    message = 'No transactions found for the given criteria.';
+  }
 
+  return { status, message, balance };
 }
 
 export default calculate;
