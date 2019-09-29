9700867be0ea7308cc1e373eb37c1855cd8b8c40
Mithun Prem <mithunprem19@gmail.com>
2019-09-29 15:57:45 +1000

    Removed the unused App.css

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import Header from './Components/Header';
 import TransactionAnalyser from './Containers/TransactionAnalyser';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
 
 function App() {
   return (
