6d9d5240f08c02c641c9ffe006923c99af7fb402
Mithun Prem <mithunprem19@gmail.com>
2019-10-01 13:52:02 +1000

    Modified the Input component to show an error in a tool tip if the hasError flag is true.

--- src/Components/Input/index.js
+++ src/Components/Input/index.js
@@ -1,14 +1,16 @@
 import React, { Fragment } from 'react';
+import { Tooltip } from 'reactstrap';
 import PropTypes from 'prop-types';
 import './input.scss';
 
-const Input = ({ type = "text", placeholder, name, value, hasError, onChange = () => { } }) => {
+const Input = ({ type = "text", placeholder, name, value, hasError, errorMessage, onChange = () => { } }) => {
   return (
     <Fragment>
       <div className="input">
         <label htmlFor={name}>{placeholder}</label>
         <br/>
         <input
+          id={name}
           placeholder={placeholder}
           className={hasError ? 'has-error' : ''}
           type={type}
@@ -17,6 +19,7 @@ const Input = ({ type = "text", placeholder, name, value, hasError, onChange = (
           onChange={onChange}
         />
       </div>
+      <Tooltip placement="top" target={name} isOpen={hasError}>{errorMessage}</Tooltip>
     </Fragment>
   )
 }
